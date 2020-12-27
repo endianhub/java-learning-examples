e4db5e8d6dc311d501e27a185a6fa7ab7064aa4a
endiancn <endianhub@users.noreply.github.com>
2020-12-28 06:20:42 +0800

    commit

--- 15_thread-volatile/src/main/java/com/xh/thread/Thread002.java
+++ 15_thread-volatile/src/main/java/com/xh/thread/Thread002.java
@@ -4,6 +4,8 @@ package com.xh.thread;
  * 当主线程修改 FLAG 值时则停止线程
  */
 public class Thread002 extends Thread {
+    // 可以保证可见性；防止重排序；不能保证原子性
+    // Lock 锁汇编指令实现，让副本数据主动刷新主内存中的数据，从而实现缓存数据一致性问题
     private static volatile boolean FLAG = true;
 
     @Override
